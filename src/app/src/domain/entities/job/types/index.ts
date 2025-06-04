827928bf24cfccf25d4cffcf317063aed1dc2ce8
Marcos D'Andrea <86210781+marcosdandrea@users.noreply.github.com>
2025-06-04 19:44:40 -0300

    Add Artnet singleton service and update SendArtnetJob

--- src/app/src/domain/entities/job/types/index.ts
+++ src/app/src/domain/entities/job/types/index.ts
@@ -1,10 +1,12 @@
 export default {
     sendUDPJob: await import("./sendUDP/index.js"),
+    sendArtnetJob: await import("./sendArtnet/index.js"),
     waitJob: await import("./wait/index.js"),
-} 
+}
 
 export const jobTypes = {
     sendUDPJob: "sendUDPJob",
     sendTCPJob: "sendTCPJob",
+    sendArtnetJob: "sendArtnetJob",
     waitJob: "waitJob",
 }
