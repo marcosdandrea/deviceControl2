3379e42ccc8ed1852d4fe3df5a210359ef18245e
Marcos D'Andrea <86210781+marcosdandrea@users.noreply.github.com>
2025-06-20 21:42:54 -0300

    fix drag speed for task cards

--- src/ui/views/NodeView/nodeTypes/RoutineNode/components/TaskCard/index.tsx
+++ src/ui/views/NodeView/nodeTypes/RoutineNode/components/TaskCard/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useSortable } from '@dnd-kit/sortable';
 import { CSS } from '@dnd-kit/utilities';
+import { useViewport } from '@xyflow/react';
 import style from './style.module.css';
 
 interface TaskCardProps {
@@ -18,9 +19,14 @@ const TaskCard: React.FC<TaskCardProps> = ({ id, containerId, content }) => {
     transition,
     isDragging,
   } = useSortable({ id, data: { containerId } });
+  const { zoom } = useViewport();
 
   const styleItem: React.CSSProperties = {
-    transform: CSS.Transform.toString(transform),
+    transform: CSS.Transform.toString({
+      ...transform,
+      x: transform ? transform.x / zoom : 0,
+      y: transform ? transform.y / zoom : 0,
+    }),
     transition,
   };
 
