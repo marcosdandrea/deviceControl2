a236ba190db7b6f0240e73081adda7f105a33608
Marcos D'Andrea <marcosdandrea@gmail.com>
2025-07-04 11:25:49 -0300

    [fix] - throwback UI. We will not using react flow

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import style from './style.module.css';
-import NodeView from '@views/NodeView';
 
 function App() {
 
     return (
         <div className={style.app}>
-           <NodeView/>
+            OK
         </div>
     );
 }
