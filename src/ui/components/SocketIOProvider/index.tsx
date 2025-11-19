2ffce018ba0f5b46f3f3d07f9e18e601d494275a
Marcos D'Andrea <marcosdandrea@gmail.com>
2025-11-19 18:08:26 -0300

    [fix] - fixed port detection in GUI

--- src/ui/components/SocketIOProvider/index.tsx
+++ src/ui/components/SocketIOProvider/index.tsx
@@ -7,10 +7,17 @@ import { Logger } from '@helpers/logger';
 export const SocketIOContext = createContext(null);
 
 const SocketIOProvider = ({ children, mountComponentsOnlyWhenConnect = false, disconnectionViewComponent }) => {
-    const socketURL = `http://${window.location.hostname}`;
+    const port = Number(window.location.port) == 5123 ? 8080 : Number(window.location.port); // Default port for main server is 8080
+    const socketURL = `http://${window.location.hostname}:${port}`;
+
+    if (port != 5123 && port != 8080) {
+        console.warn(`SocketIOProvider: Unexpected port ${port}, defaulting to 8080 for Socket.IO connection.`);
+    }
 
     const [socket, setSocket] = useState(null);
     const [isConnected, setIsConnected] = useState(false);
+
+    console.log ("Socket.IO URL:", socketURL);
 
     useEffect(() => {
         const newSocket = io(socketURL);
